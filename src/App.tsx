8bc310c56d4ff0127dcf38183067c7217fb2e113
jefferson <jeffer4533@gmail.com>
2023-06-07 13:56:07 -0500

    agregue las imagenes y las clases de las targets

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import imagen2 from "../src/img/images2.jpg";
+import imagen3 from "../src/img/images3.jpg";
+import imagen4 from "../src/img/images4.jpg";
+import imagen5 from "../src/img/images5.jpg";
+import imagen6 from "../src/img/images6.jpg";
+
+const cardsData = [
+  { id: 2, imagen: imagen2, title: "Tarjeta 2" },
+  { id: 3, imagen: imagen3, title: "Tarjeta 3" },
+  { id: 4, imagen: imagen4, title: "Tarjeta 4" },
+  { id: 5, imagen: imagen5, title: "Tarjeta 5" },
+  { id: 6, imagen: imagen6, title: "Tarjeta 6" },
+  // Agrega más objetos de tarjeta según tus necesidades
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <h2> javascript </h2>
+      {cardsData.map((card) => (
+        <div className="card card-container" key={card.id}>
+          <img src={card.imagen} alt={card.title} />
+          <div className="card-content">
+            <h3 className="card-title"> 71%Dto.</h3>
+            <p className="card-text">Contenido adicional de la tarjeta</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
-
-export default App
+export default App;
