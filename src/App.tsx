95be3fc45a13bf305e75db7bdc139b58aec5fdf9
jefferson <jeffer4533@gmail.com>
2023-06-09 15:28:15 -0500

    implemente tres componentes donde esta el titulo un banner y las cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,43 @@
+
+import React from "react";
+import Banner from "./components/Banner";
+import Title from "./components/Title";
+import Card from "./components/Card";
 import "./App.css";
+
+
 import imagen2 from "../src/img/images2.jpg";
 import imagen3 from "../src/img/images3.jpg";
 import imagen4 from "../src/img/images4.jpg";
 import imagen5 from "../src/img/images5.jpg";
-import imagen6 from "../src/img/images6.jpg";
-
-const cardsData = [
-  { id: 2, imagen: imagen2, title: "Tarjeta 2" },
-  { id: 3, imagen: imagen3, title: "Tarjeta 3" },
-  { id: 4, imagen: imagen4, title: "Tarjeta 4" },
-  { id: 5, imagen: imagen5, title: "Tarjeta 5" },
-  { id: 6, imagen: imagen6, title: "Tarjeta 6" },
-  // Agrega más objetos de tarjeta según tus necesidades
-];
 
 function App() {
+  const cardsData = [
+    { id: 2, image: imagen2, title: "Tarjeta 2" },
+    { id: 3, image: imagen3, title: "Tarjeta 3" },
+    { id: 4, image: imagen4, title: "Tarjeta 4" },
+    { id: 5, image: imagen5, title: "Tarjeta 5" },
+    { id: 6, image: imagen5, title: "Tarjeta 5" },
+  ];
+
   return (
-    <div className="container">
-      <h2> javascript </h2>
-      {cardsData.map((card) => (
-        <div className="card card-container" key={card.id}>
-          <img src={card.imagen} alt={card.title} />
-          <div className="card-content">
-            <h3 className="card-title"> 71%Dto.</h3>
-            <p className="card-text">Contenido adicional de la tarjeta</p>
-          </div>
+    
+    <div>
+      
+      <Title />
+      <Banner />
+      <div className="container">
+        <div className="card-container">
+          {cardsData.map((card) => (
+            <Card key={card.id} image={card.image} title={card.title} />
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 }
+
+
+
+
 export default App;
